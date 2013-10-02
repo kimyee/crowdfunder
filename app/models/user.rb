0198bf7badd0619d6298a9bf01db89d055bb0251
kimyee <kimyee.16@gmail.com>
2013-10-02 16:06:17 -0400

    Generate user test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ActiveRecord::Base
    attr_accessible :first_name, :last_name, :email, :password, :password_confirmation
   has_many :projects
 
-  # validates :first_name, presence: true
-  # validates :last_name, presence: true
-  # validates :email, presence: true
-  # validates :password, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
   validates_confirmation_of :password, :message => "should match confirmation", :if => :password 
 
 end
