0454078909fd677297a716cc81fadf628649a00a
kimyee <kimyee.16@gmail.com>
2013-10-03 17:41:41 -0400

    Making tests for Pledge model pass

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -2,4 +2,8 @@ class Pledge < ActiveRecord::Base
   attr_accessible :amount, :project_id, :user_id
   belongs_to :user
   belongs_to :project 
+
+	validates :user_id, presence: true  
+	validates :project_id, presence: true
+	validates :amount, numericality: {greater_than: 0, integer: true}
 end
